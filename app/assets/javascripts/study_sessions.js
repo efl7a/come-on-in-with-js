c32afa2b8da741c8b66d2e8eb420b437be5a0716
Heather Christman <efl7a@yahoo.de>
2018-05-07 11:12:10 -0400

    jquery still not catching form submit

--- app/assets/javascripts/study_sessions.js
+++ app/assets/javascripts/study_sessions.js
@@ -1,8 +1,24 @@
 $(document).ready(function () {
   $('#study_session_form').hide()
   $("#create").on("click", showForm)
+  $("#new_study_session").submit(function(e) {
+    e.preventDefault()
+    debugger
+    formSubmit()
+  })
 })
 
 function showForm() {
   $("#study_session_form").toggle()
+
+}
+
+function formSubmit() {
+  debugger
+  let values = $(this).serialize()
+  let posting = $.post('/study_sessions', values)
+  posting.done(function(resp) {
+    debugger
+  })
+
 }
--- app/controllers/study_sessions_controller.rb
+++ app/controllers/study_sessions_controller.rb
@@ -21,7 +21,11 @@ class StudySessionsController < ApplicationController
     @study_session = current_user.study_sessions.build(study_session_params)
     authorize @study_session
     if @study_session.save
-      redirect_to user_study_sessions_path(current_user)
+      respond_to do |format|
+        format.html {redirect_to user_study_sessions_path(current_user)}
+        format.json {render json: @study_session, status:201}
+      # redirect_to user_study_sessions_path(current_user)
+    end
     else
       flash[:alert] = "Study session did not save."
       render "index"
