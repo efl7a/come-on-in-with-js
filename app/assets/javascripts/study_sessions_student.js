3719bf413e4bf70601ea1c382005d520e1711fc6
Heather Christman <efl7a@yahoo.de>
2018-05-14 13:14:34 -0400

    can create attendee records

--- app/assets/javascripts/study_sessions_student.js
+++ app/assets/javascripts/study_sessions_student.js
@@ -1,23 +1,36 @@
 $(document).ready(function () {
   $('.student').on("submit", function(e) {
     e.preventDefault()
-    // May need to distinguish here if the button is "attend" or "attending"
-    attendSession(e)
+    let form = $(e.currentTarget)
+    let id = form.parent().parent().data('id')
+    if(form.children("input").attr("value") === "Attend"){
+      attendSession(form, id)
+    } else {
+      changeAttendance(form, id)
+    }
   })
 })
 
-function attendSession(e) {
-  let id = $(e.currentTarget).parent().parent().data('id')
+function attendSession(form, id) {
   $.ajax({
     type: "POST",
+    url: `/attendees/${id}`,
+
+    dataType: "json"
+  }).done(function(resp) {
+    $(this).children("input").attr("value", "Attending - Change Attendance")
+  })
+}
+
+function changeAttendance(form, id){
+  $.ajax({
+    type: "DELETE",
     url: `/attendees`,
     data: {
       study_session_id: id
     },
     dataType: "json"
   }).done(function(resp) {
-    // Need to switch attendance button
-    debugger
-
+    $(this).children("input").attr("value", "Attend")
   })
 }
--- app/controllers/attendees_controller.rb
+++ app/controllers/attendees_controller.rb
@@ -17,9 +17,12 @@ class AttendeesController < ApplicationController
   end
 
   def destroy
+    binding.pry
+    @attendee = Attendee.find(user_id: current_user.id, study_session_id: params[:study_session_id])
     authorize @attendee
     if @attendee.destroy
-      redirect_to user_study_sessions_path(current_user)
+      # redirect_to user_study_sessions_path(current_user)
+      render json: "deleted"
     else
       flash[:alert] = "Could not delete this attendance record."
       render '/welcome/index'
